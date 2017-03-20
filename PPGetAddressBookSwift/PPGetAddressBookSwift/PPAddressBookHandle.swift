946ba17f9c3ff125433df6ab4d05992129fe059e
roycms <roycms@qq.com>
2017-03-20 13:39:07 +0800

    过滤掉非手机号的联系人

--- PPGetAddressBookSwift/PPGetAddressBookSwift/PPAddressBookHandle.swift
+++ PPGetAddressBookSwift/PPGetAddressBookSwift/PPAddressBookHandle.swift
@@ -132,7 +132,9 @@ class PPAddressBookHandle {
                 // 获取号码
                 let phoneValue = ABMultiValueCopyValueAtIndex(phones, i)?.takeRetainedValue() as! String? ?? ""
                 let mobile = removeSpecialSubString(string: phoneValue)
-                model.mobileArray.append(mobile)
+                if self.isTelNumber(num: mobile as NSString){
+                    model.mobileArray.append(mobile)
+                }
                 
             }
             
@@ -190,8 +192,10 @@ class PPAddressBookHandle {
             
             // 遍历一个人的所有电话号码
             for labelValue in contact.phoneNumbers {
-                let phoneNumber = labelValue.value 
-                model.mobileArray.append(phoneNumber.stringValue)
+                let mobile = removeSpecialSubString(string: labelValue.value.stringValue)
+                if self.isTelNumber(num: mobile as NSString){
+                    model.mobileArray.append(mobile)
+                }
             }
             
             // 将联系人模型回调出去
@@ -215,7 +219,29 @@ class PPAddressBookHandle {
         
         return resultString;
     }
-    
+    func isTelNumber(num:NSString)->Bool
+    {
+        let mobile = "^1(3[0-9]|5[0-35-9]|8[025-9])\\d{8}$"
+        let  CM = "^1(34[0-8]|(3[5-9]|5[017-9]|8[278])\\d)\\d{7}$"
+        let  CU = "^1(3[0-2]|5[256]|8[56])\\d{8}$"
+        let  CT = "^1((33|53|8[09])[0-9]|349)\\d{7}$"
+        let regextestmobile = NSPredicate(format: "SELF MATCHES %@",mobile)
+        let regextestcm = NSPredicate(format: "SELF MATCHES %@",CM )
+        let regextestcu = NSPredicate(format: "SELF MATCHES %@" ,CU)
+        let regextestct = NSPredicate(format: "SELF MATCHES %@" ,CT)
+        if ((regextestmobile.evaluate(with: num) == true)
+            || (regextestcm.evaluate(with: num)  == true)
+            || (regextestct.evaluate(with: num) == true)
+            || (regextestcu.evaluate(with: num) == true))
+        {
+            return true
+        }
+        else
+        {
+            return false
+        }
+    }
+
     
     // MARK: - lazy
     @available(iOS 9.0, *)
